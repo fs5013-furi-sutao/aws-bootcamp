c5e244234ab04df806fb343c3b3ef2523aaea36f
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-11-17 18:59:39 +0900

    Change theme primary color

--- src/@rocketseat/gatsby-theme-docs/styles/theme.js
+++ src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -1,10 +1,11 @@
 export default {
   colors: {
-    primary: '#8257E6',
+    // primary: '#8257E6',
+    primary: '#ff9933',
     background: '#FFFFFF',
     shape: `#F2F2FA`,
     title: `#3D3D4D`,
     text: `#6C6C80`,
     link: `#318ce7`,
   },
-};
+}
